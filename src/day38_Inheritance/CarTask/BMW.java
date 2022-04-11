3b21db69009eda5f27dd7c0bee3788c9ec073796
kursad <kursadbal89@gmail.com>
2022-04-11 10:58:26 +0200

    day38 Inheritance

---
+++ src/day38_Inheritance/CarTask/BMW.java
@@ -0,0 +1,20 @@
+package day38_Inheritance.CarTask;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BMW extends Car{
+
+
+    public BMW( String model, int year, double price, String color, double miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+" "+ model+" "+ " is breaking down");
+    }
+
+    public void racing(){
+        System.out.println(brand + " "+ model+ " is racing car");
+    }
+
+}
---
+++ src/day38_Inheritance/CarTask/Toyota.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.CarTask;
+
+public class Toyota extends Car {
+
+    public Toyota(String model, int year, double price, String color, double miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand +" "+ " is reliable");
+    }
+
+}
